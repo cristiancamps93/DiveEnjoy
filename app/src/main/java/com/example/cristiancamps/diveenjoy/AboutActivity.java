372fe21d19ebec10cf82ed74850e2aec710759b7
Cristian <cristiancamps93@gmail.com>
2019-01-07 01:32:07 +0100

    Añadimos un easter egg en la activity about

--- app/src/main/java/com/example/cristiancamps/diveenjoy/AboutActivity.java
+++ app/src/main/java/com/example/cristiancamps/diveenjoy/AboutActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -13,12 +14,23 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
     }
 
+
     //Boton de Back
     public void onBack(View view) {
         Intent miIntent = new Intent(AboutActivity.this, MainActivity.class);
         startActivity(miIntent);
         finish();
-
     }
+
+    //Boton easter
+    public void onEaster (View view){
+        Intent miIntent = new Intent(AboutActivity.this, EasterEgg.class);
+        startActivity(miIntent);
+        finish();
+    }
+
+
+
+
 
 }
